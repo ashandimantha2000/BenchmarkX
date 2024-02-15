b0053a0edffbb3348f55edb64900630ab2947b2f
Ashan Dimantha <ashandimanthalk@gmail.com>
2024-02-15 21:53:26 +0530

    added: header

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import {
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+
+//Import CSS Styles
+import './css/style.css';
+
+
+// Import Pages
+import Dashboard from './pages/Dashboard';
 
 function App() {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+  }, [location.pathname]); // triggered on route change
+
   return (
-    <div>App</div>
-  )
+    <>
+      <Routes>
+        <Route exact path="/" element={<Dashboard />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
--- Client/src/css/tailwind.config.js
+++ Client/src/css/tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      colors: {
+        primary_pink: '#FF3CD4',
+        light_background: '#F1F5F9'
+      },
       boxShadow: {
         DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.08), 0 1px 2px 0 rgba(0, 0, 0, 0.02)',
         md: '0 4px 6px -1px rgba(0, 0, 0, 0.08), 0 2px 4px -1px rgba(0, 0, 0, 0.02)',
