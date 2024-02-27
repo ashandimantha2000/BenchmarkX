1ec3f479147498723a5358d9edd72ede3bd72414
Ashan Dimantha <ashandimanthalk@gmail.com>
2024-02-27 14:48:22 +0530

    add:a/b testing-card

--- Client/src/components/dashboard/ABTesting.jsx
+++ Client/src/components/dashboard/ABTesting.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import { GrLinkNext } from "react-icons/gr";
 
 function ABTesting() {
   return (
-    <div className="bg-slate-100 w-1/2 h-48 rounded-lg drop-shadow-xl shadow-slate-300 mt-8">
-        A/B Testing
-
+    <div className="bg-slate-100 w-1/2 h-48 rounded-lg drop-shadow-xl shadow-slate-300 mt-8 p-2 ">
+      <div className="absolute p-5">
+        <h1 className="text-2xl font-bold text-white">A/B Testing</h1>
+        <p className="text-white font-light text-sm">
+          A methodology for comparing two user interface varients against each
+          other to determine which one performs better
+        </p>
+        <br />
+        <div className="flex">
+          <button className="text-base font-bold pr-2 text-white">
+            Explore
+          </button>
+          <GrLinkNext className="mt-1" color="white" />
+        </div>
+      </div>
+      <img
+        className="items-end h-full "
+        src="../src/assets/images/dashboard/abtest-back.png"
+        alt="A/B Testing image"
+      />
     </div>
-  )
+  );
 }
 
-export default ABTesting
+export default ABTesting;
--- Client/src/components/dashboard/HeatMaps.jsx
+++ Client/src/components/dashboard/HeatMaps.jsx
@@ -3,20 +3,20 @@ import { GrLinkNext } from "react-icons/gr";
 function HeatMaps() {
   return (
     <div className="bg-slate-100 w-1/2 h-48 rounded-lg drop-shadow-xl shadow-slate-300 mt-8 mr-11 p-3 flex">
-      <div className="pt-5 px-5">
+      <div className="pl-5 py-5 pr-3">
         <p className="text-primary_blue font-medium">BenchmarkX</p>
         <h1 className="text-2xl font-bold">HeatMaps</h1>
         <p className="text-primary_blue font-light text-sm">
-          Lorem ipsum dolor sit amet consectetur. 
+          2D data visualization technique that represents user interaction in
+          the web app.
         </p>
-        <br />
         <div className="flex">
           <button className="text-base font-bold pr-2">Explore</button>
           <GrLinkNext className="mt-1" />
         </div>
       </div>
-      <div></div>
       <img
+        className="items-end h-full"
         src="../src/assets/images/dashboard/heatmapRocket.png"
         alt="heatmap-rocket"
       />
