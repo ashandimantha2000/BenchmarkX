27084348ae1b564a474c0a47dcad49c44701883f
Ashan Dimantha <ashandimanthalk@gmail.com>
2024-05-02 11:29:39 +0530

    add: signin

--- Client/src/pages/Dashboard.jsx
+++ Client/src/pages/Dashboard.jsx
@@ -25,7 +25,7 @@ function Dashboard() {
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
             {/* Welcome banner */}
             <WelcomeBanner />
-            <h1>Kodukara Impressions</h1>
+            <h1>Nortion</h1>
             <UpdateCard />
             <div className="flex justify-between w-full">
               <HeatMaps />
