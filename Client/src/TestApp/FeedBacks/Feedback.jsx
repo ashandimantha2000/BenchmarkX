219debac0b8340b69b927aad8feb08168375007a
Ashan Dimantha <ashandimanthalk@gmail.com>
2024-05-04 22:19:28 +0530

    fix: position

--- Client/src/TestApp/FeedBacks/Feedback.jsx
+++ Client/src/TestApp/FeedBacks/Feedback.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Feedback() {
   return (
-    <div className="absolute p-10 h-screen flex justify-end  items-end">
+    <div className="absolute p-9 bottom-0 right-0 flex">
       <a href="/test/feedback" target="_blank">
         <button className="w-full bg-gradient-to-r from-sky-400 to-blue-600 hover:scale-105 text-white font-bold py-2 px-6 rounded-t-xl rounded-l-xl">
           Feedback?
