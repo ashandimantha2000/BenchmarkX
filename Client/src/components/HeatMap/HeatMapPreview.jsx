54445be8788d940244c793d719ad525c26378915
Ashan Dimantha <ashandimanthalk@gmail.com>
2024-05-12 18:46:55 +0530

    add: color picker

--- Client/src/components/HeatMap/HeatMapPreview.jsx
+++ Client/src/components/HeatMap/HeatMapPreview.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
 import HeatMap from "react-heatmap-grid";
+import { SketchPicker } from "react-color";
+import tinycolor from "tinycolor2";
 
 const HeatMapPreview = () => {
   const [movements, setMovements] = useState([]);
+  const [color, setColor] = useState("#ffffff");
 
   useEffect(() => {
-    fetch('http://localhost:5555/heatmap')
-      .then(response => response.json())
-      .then(data => setMovements(data))
+    fetch("http://localhost:5555/heatmap")
+      .then((response) => response.json())
+      .then((data) => setMovements(data))
       .catch((error) => {
-        console.error('Error:', error);
+        console.error("Error:", error);
       });
   }, []);
 
@@ -25,14 +28,66 @@ const HeatMapPreview = () => {
   // Populate the heatmap data array based on the mouse movements
   movements.forEach(({ x, y }) => {
     const xIndex = Math.floor(x / (window.innerWidth / xLabels.length));
-    const yIndex = Math.floor(y / (window.innerHeight / (yLabels.length-6)));
+    const yIndex = Math.floor(y / (window.innerHeight / (yLabels.length - 6)));
     data[yIndex][xIndex]++;
   });
 
+  const handleColorChange = (color) => {
+    setColor(color.hex);
+  };
+
+  const rgbColor = tinycolor(color).toRgb();
+
+  const [showPicker, setShowPicker] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowPicker(!showPicker);
+  };
+
+  const handleOutsideClick = (event) => {
+    if (event.target.closest(".picker-container")) return;
+    setShowPicker(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
   return (
     <div className="w-8/12 h-5">
-      <div >
-        <HeatMap xLabels={xLabels} yLabels={yLabels} data={data} />
+      <div className="relative">
+        <button
+          onClick={handleButtonClick}
+          className="absolute top-0 right-0 ml-7 w-fit px-3 py-2 bg-gradient-to-r from-sky-400 to-blue-600 hover:scale-105 text-white font-bold rounded-t-xl rounded-r-xl"
+        >
+          Colour Picker
+        </button>
+        <div className="absolute top-0 right-0">
+          {showPicker && (
+            <div className="picker-container">
+              <SketchPicker
+                color={color}
+                onChangeComplete={handleColorChange}
+              />
+            </div>
+          )}
+        </div>
+      </div>
+      <div>
+        <HeatMap
+          xLabels={xLabels}
+          yLabels={yLabels}
+          data={data}
+          cellStyle={(background, value, min, max, data, x, y) => ({
+            background: `rgba(${rgbColor.r}, ${rgbColor.g}, ${rgbColor.b}, ${
+              1 - (max - value) / (max - min)
+            })`,
+            fontSize: "11.5px",
+          })}
+        />
       </div>
     </div>
   );
